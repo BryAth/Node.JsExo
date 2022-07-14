e10910b97288f00b6f91fe1bc7146eab7838116b
Bryan Lequeux <Lequeux.Bryan95@gmail.com>
2022-07-14 10:01:56 +0200

    Deuxième push.

--- Modules/Simple/log.js
+++ Modules/Simple/log.js
@@ -1,12 +1,58 @@
 let log = {
-    info : function(infoMsg){
-        console.log(`Info : ${infoMsg}`);
+
+    ex1 : function(){
+        var dateajd = Date.now();
+        var dateNoel = new Date("12/25/2022")
+        
+        var Difference_In_Time = dateNoel - dateajd;
+
+        
+        var Difference_In_Days = Math.ceil(Difference_In_Time / (1000 * 3600 * 24));
+        console.log(`Il y a encore  ${Difference_In_Days} jours à attendre  pour fêter noel !  `)
+    },
+
+
+
+    ex2 : function(){
+        var dateajds = new Date().getTime();
+        var dateAnnif = new Date(2022,07,14).getTime();
+        var Diff = (dateAnnif - dateajds);
+
+        var Diff2 = Math.round(Diff /(1000*3600*24))
+        console.log(`Les jours entre la date d'aujourd'hui et ma date d'anniversaire est : ${Diff2}`)
+        
     } ,
-    warning : function(warningMsg){
-        console.log(`Warning : ${warningMsg}`);
+
+
+
+    ex3 : function(){
+        var tdayDate = Date.now()
+        var dateVacances = new Date("09/30/2022")
+
+        var dateUntilVacation = (dateVacances - tdayDate);
+        var resultat = Math.round(dateUntilVacation / (1000*3600*24))
+        console.log(`Nombre de jours avant les vacances : ${resultat}`)
     }  , 
-    error : function(errorMsg){
-        console.log(`Error : ${errorMsg}`)
-    }
+
+    ex4 : function(){
+        var vendrediTreize = new Date(2023,00,13);
+                const blabla = vendrediTreize.getDay();
+                const number = vendrediTreize.getDate();
+        const ajd = new Date()
+        
+
+
+        var CalculFriday13 = vendrediTreize - ajd ;
+        var Transformation = Math.round(CalculFriday13 / (1000*3600*24))
+
+        console.log("Il reste " +  Transformation + " jours avant le prochain vendredi 13.") ;
+        if(blabla == 5 && number == 13){
+            console.log("C'est un vendredi 13.");
+        }
+        else{
+            console.log("C'est un jour normal.");
+        }
+    },
+        
 }
 module.exports = log;
--- app.js
+++ app.js
@@ -1,20 +1,25 @@
 // Pour utiliser notre module log de module simple (objet)
-const logModule = require ("./Modules/Simple/log");
-//Ici logModule contient l'objet log que nous avons exporté pour accéder à chacune de ses propriétés
-logModule.info('Projet Node.JS lancé avec succès')
-logModule.warning('Attention votre projet est sur le point d\'exploser')
-logModule.error('Vous avez explosé')
+// const logModule = require ("./Modules/Simple/log");
+// //Ici logModule contient l'objet log que nous avons exporté pour accéder à chacune de ses propriétés
+// logModule.info('Projet Node.JS lancé avec succès')
+// logModule.warning('Attention votre projet est sur le point d\'exploser')
+// logModule.error('Vous avez explosé')
 
+const exo1 = require("./Modules/Simple/log")
 
+exo1.ex1()
+exo1.ex2()
+exo1.ex3()
+exo1.ex4()
 //Pour utiliser notre module log de module de fonction
-const logModuleFunc = require('./Modules/Func/log')
-//Ici logModuleFunc contient une fonctio,qu'on devra directement appeler avec les parenthèses
-logModuleFunc('Bonjour les Full Stack')
+// const logModuleFunc = require('./Modules/Func/log')
+// //Ici logModuleFunc contient une fonctio,qu'on devra directement appeler avec les parenthèses
+// logModuleFunc('Bonjour les Full Stack')
 
 
-//Pour utiliser notre module person.js ,c'est un module de classe
+// //Pour utiliser notre module person.js ,c'est un module de classe
 
-const person = require('./Modules/ClassM/Person')
+// const person = require('./Modules/ClassM/Person')
 
-let bryan = new person('Lequeux' ,'Bryan' , new Date(1995,8,14))
-console.log(bryan.description)
+// let bryan = new person('Lequeux' ,'Bryan' , new Date(1995,8,14))
+// console.log(bryan.description)
